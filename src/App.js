9bd31a7f70d66fe7d661dd3ecba1186facad0ef1
marvel <maribilisarri@gmail.com>
2025-01-13 03:00:09 +0200

    api-spiderman-search

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import './App.css';
+import { useEffect, usseEffect} from 'react';
+//87541b7b want ti fetch data from api as soon as component loads so useEFFET
 
-function App() {
+const API_URL = 'http://omdbapi.com?apikey=87541b7b'
+
+const App = () => {
+  const searchMovies = async (title) =>{
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data);
+
+  }
+
+  useEffect(() => {
+    searchMovies('Spiderman');
+
+  },[]);
+
   return (
     <div className="App">
       <h1>hi</h1>
